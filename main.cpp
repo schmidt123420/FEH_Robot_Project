787cf76fca95d2e3e1ff27cb404a211a5029e288
Girard <girard.73@coeit.osu.edu>
2022-03-29 18:46:20 -0400

    Added initial main.cpp code

--- main.cpp
+++ main.cpp
@@ -13,6 +13,47 @@
 
 
 int main(void) {
+//bottomServo.TouchCalibrate();
 
-    return 0;
+    int motorPercent = 40;
+    int expectedCountsPerInch = 318/(WHEEL_DIAMETER * PI); //Input theoretical counts
+
+    bottomServo.SetMin(BOTTOM_SERVO_MIN);
+    bottomServo.SetMax(BOTTOM_SERVO_MAX);
+    middleServo.SetMin(MIDDLE_SERVO_MIN);
+    middleServo.SetMax(MIDDLE_SERVO_MAX);
+    topServo.SetMin(TOP_SERVO_MIN);
+    topServo.SetMax(TOP_SERVO_MAX);
+    //Start with arm in upright starting position
+    setArmStart();
+
+    //Wait until light turns on
+    int firstLight = waitUntilLight();
+
+    //Print to LCD that sees light
+    if (firstLight == BLUE_COLOR) {
+        LCD.WriteLine("Light color: Blue");
+    }
+    else {
+        LCD.WriteLine("Light color: Red");
+    }
+
+    // Move forward 13.5 inches
+    moveDistance(35,1,13.5*EXPECTED_COUNTS_PER_INCH);
+    // Turn 45 right
+    Sleep(.5);
+    turnRight(25,45);
+    // Move forward 35 inches (up the ramp)
+    moveDistance(45,1, 35*EXPECTED_COUNTS_PER_INCH);
+    turnLeft(25,90);
+    moveDistance(25,1,2*EXPECTED_COUNTS_PER_INCH);
+    Sleep(.5);
+    turnLeft(25,10);
+    Sleep(.5);
+    // int t = TimeNow();
+    // while(t<TimeNow()+2.0){
+    //      driveUntilLine();
+    //  }
+    followLineForDistance(2);
+    dropTray();
 }
