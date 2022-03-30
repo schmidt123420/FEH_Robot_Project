eb2481307463dc8886a059868179ea4b16fbbbc2
Matt Schmidt <schmidt.1234@osu.edu>
2022-03-30 16:05:33 -0400

    made wheel diameter a float

--- roboconstants.h
+++ roboconstants.h
@@ -10,11 +10,7 @@
 */
 #define PI 3.141592653589793
 #define BOT_WIDTH 9 //width of crayola bot
-<<<<<<< HEAD
 #define WHEEL_DIAMETER 3 //diameter of robot wheel
-=======
-#define WHEEL_DIAMETER 3.0 //diameter of robot wheel
->>>>>>> 41f53b10f7a6389138bdbee4b805720b8b62fd99
 #define BLUE_COLOR 0
 #define RED_COLOR 1
 #define FORWARD true
@@ -30,11 +26,7 @@
 #define RIGHT_OPT_THRESHOLD 2.3
 #define MIDDLE_OPT_THRESHOLD 2.3
 #define LEFT_OPT_THRESHOLD 2.4
-<<<<<<< HEAD
 #define EXPECTED_COUNTS_PER_INCH 318.0/(WHEEL_DIAMETER * PI)
-=======
-#define EXPECTED_COUNTS_PER_INCH (318.0/(WHEEL_DIAMETER * PI))
->>>>>>> 41f53b10f7a6389138bdbee4b805720b8b62fd99
 
 //Line states for line following algorithim
 enum LineStates {
